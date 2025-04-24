c436b2d92018a88ebcddac66a331c12480d4698d
Mandeep Singhmar <msingh8328@gmail.com>
2025-04-24 12:22:23 +0530

    fix: remove uneccessary imports

--- src/components/dashboard/InventoryChart.tsx
+++ src/components/dashboard/InventoryChart.tsx
@@ -11,7 +11,6 @@ import {
   Legend,
   ResponsiveContainer,
 } from 'recharts';
-import clsx from 'clsx';
 import { weeklyData } from '@/utils/data';
 import ToggleButton from '../ToggleButton';
 
@@ -36,29 +35,6 @@ const InventoryChart = () => {
           </p>
         </div>
         <div className='flex items-center space-x-1 rounded-full bg-gray-100 p-1 border-[0.92px] border-[#E5E5E5] '>
-          {/* <button
-            onClick={() => setTimeframe('DAILY')}
-            className={clsx(
-              'rounded-full px-5 py-1 text-xs cursor-pointer font-bold transition-colors',
-              timeframe === 'DAILY'
-                ? 'bg-[#4F45E4] text-white shadow-sm'
-                : 'text-gray-500 hover:text-gray-700'
-            )}
-          >
-            Daily
-          </button>
-          <button
-            onClick={() => setTimeframe('WEEKLY')}
-            className={clsx(
-              'rounded-full px-5 py-1 text-xs font-bold cursor-pointer transition-colors',
-              timeframe === 'WEEKLY'
-                ? 'bg-[#4F45E4]  text-white shadow-sm'
-                : 'text-gray-500 hover:text-gray-700'
-            )}
-          >
-            Weekly
-          </button> */}
-
           <ToggleButton
             isActive={timeframe === 'DAILY'}
             onClick={() => setTimeframe('DAILY')}
